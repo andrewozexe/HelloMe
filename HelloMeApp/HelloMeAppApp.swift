08e186b24a055dd1a38db1d5f45cbbe7ea525a9c
andre wozniack <andre.wozniack@pucpr.edu.br>
2023-03-09 11:14:15 -0300

    Initial Commit

---
+++ HelloMeApp/HelloMeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloMeAppApp.swift
+//  HelloMeApp
+//
+//  Created by André Wozniack on 09/03/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloMeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
